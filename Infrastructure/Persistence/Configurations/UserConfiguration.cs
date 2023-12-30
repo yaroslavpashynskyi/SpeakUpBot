ae59a16c5bd896ff7dc692694c6a4c7e35e93f40
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2023-12-30 17:40:44 +0200

    Make user telegram id to be unique

---
+++ Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasIndex(u => u.TelegramId).IsUnique();
+    }
+}
