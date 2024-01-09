d91214ea6fd2e6d7638d9e5c014895e432f49272
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2024-01-09 20:56:38 +0200

    Prevent always using a new NpgsqlDataSourceBuilder

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -22,25 +22,22 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("DefaultConnection");
 
         services.AddTransient<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        var dataSourceBuilder = new Npgsql.NpgsqlDataSourceBuilder(connectionString);
 
+        dataSourceBuilder.MapEnum<EnglishLevel>();
+        dataSourceBuilder.MapEnum<PaymentStatus>();
+        dataSourceBuilder.MapEnum<Role>();
+
+        var dataSource = dataSourceBuilder.Build();
         services.AddDbContext<ApplicationDbContext>(
             (sp, opt) =>
             {
-                var dataSourceBuilder = new Npgsql.NpgsqlDataSourceBuilder(connectionString);
-
-                dataSourceBuilder.MapEnum<EnglishLevel>();
-                dataSourceBuilder.MapEnum<PaymentStatus>();
-                dataSourceBuilder.MapEnum<Role>();
-
                 opt.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
-
-                var dataSource = dataSourceBuilder.Build();
                 opt.UseNpgsql(dataSource);
             },
             contextLifetime: ServiceLifetime.Transient,
             optionsLifetime: ServiceLifetime.Transient
         );
-        //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         services.AddTransient<IApplicationDbContext>(
             provider => provider.GetRequiredService<ApplicationDbContext>()
         );
