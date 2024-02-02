79b662ea12f0bf4acaddffb274517d598bcb8472
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2024-02-02 23:58:13 +0200

    Add PaidByTransferTicket and describe all payment statuses

--- Domain/Enums/PaymentStatus.cs
+++ Domain/Enums/PaymentStatus.cs
@@ -1,10 +1,24 @@
-﻿namespace Domain.Enums;
+﻿using System.ComponentModel;
+
+namespace Domain.Enums;
 
 public enum PaymentStatus
 {
+    [Description("Очікується оплата")]
     Pending,
+
+    [Description("Оплата повинна бути підтверджена організатором")]
     ToBeApproved,
+
+    [Description("Оплачено картою")]
     PaidByCard,
+
+    [Description("Оплачено квитком переносу")]
+    PaidByTransferTicket,
+
+    [Description("Оплачено/Буде оплачено готівкою")]
     ToBePaidByCash,
+
+    [Description("Скасовано")]
     Cancelled
 }
---
+++ Infrastructure/Persistence/Migrations/20240202214449_AddPaidByTransferTicker.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaidByTransferTicker : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:english_level", "pre_intermediate,intermediate,upper_intermediate,advanced")
+                .Annotation("Npgsql:Enum:payment_status", "pending,to_be_approved,paid_by_card,paid_by_transfer_ticket,to_be_paid_by_cash,cancelled")
+                .Annotation("Npgsql:Enum:role", "member,admin")
+                .OldAnnotation("Npgsql:Enum:english_level", "pre_intermediate,intermediate,upper_intermediate,advanced")
+                .OldAnnotation("Npgsql:Enum:payment_status", "pending,to_be_approved,paid_by_card,to_be_paid_by_cash,cancelled")
+                .OldAnnotation("Npgsql:Enum:role", "member,admin");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:english_level", "pre_intermediate,intermediate,upper_intermediate,advanced")
+                .Annotation("Npgsql:Enum:payment_status", "pending,to_be_approved,paid_by_card,to_be_paid_by_cash,cancelled")
+                .Annotation("Npgsql:Enum:role", "member,admin")
+                .OldAnnotation("Npgsql:Enum:english_level", "pre_intermediate,intermediate,upper_intermediate,advanced")
+                .OldAnnotation("Npgsql:Enum:payment_status", "pending,to_be_approved,paid_by_card,paid_by_transfer_ticket,to_be_paid_by_cash,cancelled")
+                .OldAnnotation("Npgsql:Enum:role", "member,admin");
+        }
+    }
+}
