954640d8c233448e215782eacd0c7a2580012118
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2024-02-02 23:59:51 +0200

    Create registration command

---
+++ Application/Registrations/Commands/CreateRegistration/CreateRegistrationCommand.cs
@@ -0,0 +1,66 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+
+using Domain.Common;
+using Domain.Entities;
+using Domain.Enums;
+
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Registrations.Commands.CreateRegistration;
+
+public class CreateRegistrationCommand : IRequest<Result<Registration, Error>>
+{
+    public Speaking Speaking { get; set; } = null!;
+    public long UserTelegramId { get; set; }
+}
+
+public class CreateRegistrationCommandHandler
+    : IRequestHandler<CreateRegistrationCommand, Result<Registration, Error>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public CreateRegistrationCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Result<Registration, Error>> Handle(
+        CreateRegistrationCommand request,
+        CancellationToken cancellationToken
+    )
+    {
+        var user = await _context.Users
+            .Include(u => u.Registrations)
+            .FirstOrDefaultAsync(u => u.TelegramId == request.UserTelegramId);
+        if (user == null)
+            return NotFoundErrors<User>.EntityNotFound;
+
+        var speaking = await _context.Speakings.FirstOrDefaultAsync(
+            s => s.Id == request.Speaking.Id
+        );
+        if (speaking == null)
+            return NotFoundErrors<Speaking>.EntityNotFound;
+
+        var isRegistrationDuplicate = await _context.Registrations.AnyAsync(
+            r => r.Speaking.Id == speaking.Id && r.User.Id == user.Id
+        );
+
+        if (isRegistrationDuplicate)
+            return RegistrationErrors.RegistrationDuplication;
+
+        var registration = new Registration() { Speaking = speaking, User = user };
+        if (user.TransferTicket)
+        {
+            user.TransferTicket = false;
+            registration.PaymentStatus = PaymentStatus.PaidByTransferTicket;
+        }
+
+        await _context.Registrations.AddAsync(registration);
+
+        await _context.SaveChangesAsync(cancellationToken);
+        return registration;
+    }
+}
---
+++ Application/Registrations/RegistrationErrors.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Common;
+
+namespace Application.Registrations;
+
+public static class RegistrationErrors
+{
+    public static readonly Error RegistrationDuplication =
+        new(
+            "Registration.RegistrationDuplication",
+            "Реєстрація на даний спікінг від користувача вже існує"
+        );
+}
