b7a2db8857bad13cecf0c1df02950da1cebd8b57
Yaroslav Pashynskyi <yaroslav.pashynskyi@gmail.com>
2024-02-10 23:59:16 +0200

    Fix EntityNotFound Error not displaying the name of entity

--- Application/Common/Models/NotFoundErrors.cs
+++ Application/Common/Models/NotFoundErrors.cs
@@ -5,5 +5,5 @@ namespace Application.Common.Models;
 public static class NotFoundErrors<T>
 {
     public static readonly Error EntityNotFound =
-        new($"{nameof(T)}.NotFound", $"Сутність {nameof(T)} не знайдена");
+        new($"{typeof(T).Name}.NotFound", $"Сутність {typeof(T).Name} не знайдена");
 }
